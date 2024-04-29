e7ce15430e66f3ac3c857af75b6828239a0a8921
Prathmesh <prathmeshparab12.o@gmail.com>
2024-04-29 23:08:11 +0530

    Done with day 6

--- array_revision/basics_array.cpp
+++ array_revision/basics_array.cpp
@@ -451,6 +451,63 @@ int longestSubarraywithSumK(vector<int> arr, int k)
     return maxiLen;
 }
 
+// for negative array element
+int getLongestSubarray(vector<int> &a, int k)
+{
+
+    // Brute Force
+    //  int n = a.size();
+
+    // int len = 0;
+    // for (int i = 0; i < n; i++) { /
+    //     int s = 0;
+    //     for (int j = i; j < n; j++) { // ending index
+
+    //         s += a[j];
+
+    //         if (s == k)
+    //             len = max(len, j - i + 1);
+    //     }
+    // }
+    // return len;
+
+    // Optimal
+
+    int n = a.size(); // size of the array.
+
+    map<int, int> preSumMap;
+    int sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++)
+    {
+        // calculate the prefix sum till index i:
+        sum += a[i];
+
+        // if the sum = k, update the maxLen:
+        if (sum == k)
+        {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        // calculate the sum of remaining part i.e. x-k:
+        int rem = sum - k;
+
+        // Calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end())
+        {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        // Finally, update the map checking the conditions:
+        if (preSumMap.find(sum) == preSumMap.end())
+        {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
 int main()
 {
     vector<int> arr = {12, 44, 55, 222};
@@ -492,9 +549,14 @@ int main()
     // vector<int> num3 = {0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0};
     // cout << maximumConsecutiveOne(num3);
 
-    vector<int> v = {4, 1, 1, 3, 3, 2, 2};
-    vector<int> v1 = {2, 3, 5, 1, 9};
-    // cout << getSingleElement(v);
-    cout << longestSubarraywithSumK(v1, 10);
+    // vector<int> v = {4, 1, 1, 3, 3, 2, 2};
+    // vector<int> v1 = {2, 3, 5, 1, 9};
+    // // cout << getSingleElement(v);
+    // cout << longestSubarraywithSumK(v1, 10);
+
+    vector<int> a = {-1, 1, 1};
+    int k = 1;
+    int len = getLongestSubarray(a, k);
+    cout << "The length of the longest subarray is: " << len << "\n";
     return 0;
 }
