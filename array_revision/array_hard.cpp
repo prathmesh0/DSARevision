ba0e429d457b58b4816bffa4715c491e6d502637
Prathmesh <prathmeshparab12.o@gmail.com>
2024-05-09 00:51:06 +0530

    Done

--- array_revision/array_hard.cpp
+++ array_revision/array_hard.cpp
@@ -98,14 +98,119 @@ vector<int> majorityElement(vector<int> &arr)
     return ans;
 }
 
+vector<vector<int>> triplet(int n, vector<int> &arr)
+{
+
+    // Brute force approach
+
+    // set<vector<int>> st;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j < n; j++)
+    //     {
+    //         for (int k = j + 1; k < n; k++)
+    //         {
+
+    //             if (arr[i] + arr[j] + arr[k] == 0)
+    //             {
+    //                 vector<int> temp = {arr[i], arr[j], arr[k]};
+    //                 sort(temp.begin(), temp.end());
+    //                 st.insert(temp);
+    //             }
+    //         }
+    //     }
+    // }
+    // vector<vector<int>> ans(st.begin(), st.end());
+    // return ans;
+
+    // better Approach
+    // set<vector<int>> st;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     set<int> hashst;
+    //     for (int j = i + 1; j < n; j++)
+    //     {
+    //         int rem = -(arr[j] + arr[i]);
+    //         if (hashst.find(rem) != hashst.end())
+    //         {
+    //             vector<int> temp = {arr[i], arr[j], rem};
+    //             sort(temp.begin(), temp.end());
+    //             st.insert(temp);
+    //         }
+
+    //         hashst.insert(arr[j]);
+    //     }
+    // }
+    // vector<vector<int>> ans(st.begin(), st.end());
+    // return ans;
+
+    // Optimal Approach
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; i++)
+    {
+
+        if (i != 0 && arr[i] == arr[i - 1])
+            continue;
+
+        int j = i + 1;
+        int k = n - 1;
+        while (j < k)
+        {
+            int sum = arr[i] + arr[j] + arr[k];
+            if (sum < 0)
+            {
+                j++;
+            }
+            else if (sum > 0)
+            {
+                k--;
+            }
+            else
+            {
+                vector<int> temp = {arr[i], arr[j], arr[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                // skip the duplicates:
+                while (j < k && arr[j] == arr[j - 1])
+                    j++;
+                while (j < k && arr[k] == arr[k + 1])
+                    k--;
+            }
+        }
+    }
+    return ans;
+}
 
 int main()
 {
-    vector<int> arr = {11, 33, 33, 11, 33, 11};
-    vector<int> ans = majorityElement(arr);
-    cout << "The majority elements are: ";
+    // {
+    //     vector<int> arr = {11, 33, 33, 11, 33, 11};
+    //     vector<int> ans = majorityElement(arr);
+    //     cout << "The majority elements are: ";
+    //     for (auto it : ans)
+    //         cout << it << " ";
+    //     cout << "\n";
+
+    vector<int> arr1 = {-1,
+                        0,
+                        1,
+                        2,
+                        -1,
+                        -4};
+    int n = arr1.size();
+    vector<vector<int>> ans = triplet(n, arr1);
     for (auto it : ans)
-        cout << it << " ";
+    {
+        cout << "[";
+        for (auto i : it)
+        {
+            cout << i << " ";
+        }
+        cout << "] ";
+    }
     cout << "\n";
     return 0;
 }
