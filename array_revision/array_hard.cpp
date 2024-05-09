6b42df279c024e278fdb4f5659d0d7d3b2c0be53
Prathmesh <prathmeshparab12.o@gmail.com>
2024-05-09 23:48:43 +0530

    Done with the day 17

--- array_revision/array_hard.cpp
+++ array_revision/array_hard.cpp
@@ -184,6 +184,160 @@ vector<vector<int>> triplet(int n, vector<int> &arr)
     return ans;
 }
 
+int solve(vector<int> &arr)
+{
+    int n = arr.size();
+    // int maxLength = 0;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int sum = 0;
+    //     for (int j = i; j < n; j++)
+    //     {
+    //         sum += arr[j];
+    //         if (sum == 0)
+    //         {
+    //             maxLength = max(maxLength, j - i + 1);
+    //         }
+    //     }
+    // }
+    // return maxLength;
+
+    int maxLen = 0;
+    unordered_map<int, int> mpp;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+
+        if (sum == 0)
+        {
+            maxLen = i + 1;
+        }
+        else if (mpp.find(sum) != mpp.end())
+        {
+            maxLen = max(maxLen, i - mpp[sum]);
+        }
+        else
+        {
+            mpp[sum] = i;
+        }
+    }
+    return maxLen;
+}
+
+vector<vector<int>> fourSum(vector<int> &nums, int target)
+{
+    // Brute force approach
+    //  int n = nums.size();
+    //  set<vector<int>> st;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j < n; j++)
+    //     {
+    //         for (int k = j + 1; k < n; k++)
+    //         {
+    //             for (int l = k + 1; l < n; l++)
+    //             {
+    //                 long long sum = nums[i] + nums[j];
+    //                 sum += nums[k];
+    //                 sum += nums[l];
+
+    //                 if (sum == target)
+    //                 {
+    //                     vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+    //                     sort(temp.begin(), temp.end());
+    //                     st.insert(temp);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // vector<vector<int>> ans(st.begin(), st.end());
+    // return ans;
+
+    // Better Approach
+    // int n = nums.size();
+    // set<vector<int>> st;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j < n; j++)
+    //     {
+    //         set<long long> hashSet;
+    //         for (int k = j + 1; k < n; k++)
+    //         {
+    //             long long sum = nums[i] + nums[j];
+    //             sum += nums[k];
+
+    //             long long rem = target - sum;
+    //             if (hashSet.find(rem) != hashSet.end())
+    //             {
+    //                 vector<int> temp = {nums[i], nums[j], nums[k], (int)rem};
+    //                 sort(temp.begin(), temp.end());
+    //                 st.insert(temp);
+    //             }
+    //             hashSet.insert(nums[k]);
+    //         }
+    //     }
+    // }
+    // vector<vector<int>> ans(st.begin(), st.end());
+    // return ans;
+
+    // Optimal Approach
+    int n = nums.size(); // size of the array
+    vector<vector<int>> ans;
+
+    // sort the given array:
+    sort(nums.begin(), nums.end());
+
+    // calculating the quadruplets:
+    for (int i = 0; i < n; i++)
+    {
+        // avoid the duplicates while moving i:
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+        for (int j = i + 1; j < n; j++)
+        {
+            // avoid the duplicates while moving j:
+            if (j > i + 1 && nums[j] == nums[j - 1])
+                continue;
+
+            // 2 pointers:
+            int k = j + 1;
+            int l = n - 1;
+            while (k < l)
+            {
+                long long sum = nums[i];
+                sum += nums[j];
+                sum += nums[k];
+                sum += nums[l];
+                if (sum == target)
+                {
+                    vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                    ans.push_back(temp);
+                    k++;
+                    l--;
+
+                    // skip the duplicates:
+                    while (k < l && nums[k] == nums[k - 1])
+                        k++;
+                    while (k < l && nums[l] == nums[l + 1])
+                        l--;
+                }
+                else if (sum < target)
+                    k++;
+                else
+                    l--;
+            }
+        }
+    }
+
+    return ans;
+}
+
 int main()
 {
     // {
@@ -194,20 +348,37 @@ int main()
     //         cout << it << " ";
     //     cout << "\n";
 
-    vector<int> arr1 = {-1,
-                        0,
-                        1,
-                        2,
-                        -1,
-                        -4};
-    int n = arr1.size();
-    vector<vector<int>> ans = triplet(n, arr1);
+    // vector<int> arr1 = {-1,
+    //                     0,
+    //                     1,
+    //                     2,
+    //                     -1,
+    //                     -4};
+    // int n = arr1.size();
+    // vector<vector<int>> ans = triplet(n, arr1);
+    // for (auto it : ans)
+    // {
+    //     cout << "[";
+    //     for (auto i : it)
+    //     {
+    //         cout << i << " ";
+    //     }
+    //     cout << "] ";
+    // }
+    // cout << "\n";
+    // vector<int> a = {9, -3, 3, -1, 6, -5};
+    // cout << solve(a) << endl;
+
+    vector<int> nums = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
+    int target = 9;
+    vector<vector<int>> ans = fourSum(nums, target);
+    cout << "The quadruplets are: \n";
     for (auto it : ans)
     {
         cout << "[";
-        for (auto i : it)
+        for (auto ele : it)
         {
-            cout << i << " ";
+            cout << ele << " ";
         }
         cout << "] ";
     }
