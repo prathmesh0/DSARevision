6d93d6331a2ebd7bbf56f007f4b89de0f2f4462b
Prathmesh <prathmeshparab12.o@gmail.com>
2024-05-14 23:49:40 +0530

    Done with day 21

--- array_revision/array_hard.cpp
+++ array_revision/array_hard.cpp
@@ -684,6 +684,83 @@ void Rotatetoright(int arr[], int n, int k)
     // Reverse whole array
     Reverse(arr, 0, n - 1);
 }
+int merge(vector<int> &arr, int low, int mid, int high)
+{
+    vector<int> temp;    // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1; // starting index of right half of arr
+
+    // Modification 1: cnt variable to count the pairs:
+    int cnt = 0;
+
+    // storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            cnt += (mid - left + 1); // Modification 2
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+
+    return cnt;
+}
+
+int mergeSort(vector<int> &arr, int low, int high)
+{
+    int cnt = 0;
+    if (low >= high)
+        return cnt;
+    int mid = (low + high) / 2;
+    cnt += mergeSort(arr, low, mid);      // left half
+    cnt += mergeSort(arr, mid + 1, high); // right half
+    cnt += merge(arr, low, mid, high);    // merging sorted halves
+    return cnt;
+}
+
+int numberOfInversions(vector<int> &a, int n)
+{
+
+    // Brute force approach
+    // int cnt = 0;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j < n; j++)
+    //     {
+    //         if (a[i] > a[j])
+    //             cnt++;
+    //     }
+    // }
+    // return cnt;
+    return mergeSort(a, 0, n - 1);
+}
 int main()
 {
     // {
@@ -784,14 +861,21 @@ int main()
     //     cout << "\n";
     // }
 
-    int n = 7;
-    int arr[] = {1, 2, 3, 4, 5, 6, 7};
-    int k = 2;
-    Rotatetoright(arr, n, k);
-    cout << "After Rotating the elements to right " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
+    // int n = 7;
+    // int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    // int k = 2;
+    // Rotatetoright(arr, n, k);
+    // cout << "After Rotating the elements to right " << endl;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    vector<int> a = {5, 4, 3, 2, 1};
+    int n = 5;
+    int cnt = numberOfInversions(a, n);
+    cout << "The number of inversions is: "
+         << cnt << endl;
+
     return 0;
 }
