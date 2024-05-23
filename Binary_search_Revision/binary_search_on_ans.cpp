c4017916c135c5babe1abb7699bd977d16e2165d
Prathmesh <prathmeshparab12.o@gmail.com>
2024-05-23 21:23:10 +0530

    Done with day 31

--- Binary_search_Revision/binary_search_on_ans.cpp
+++ Binary_search_Revision/binary_search_on_ans.cpp
@@ -57,11 +57,85 @@ int minimumRateToEatBananas(vector<int> arr, int h)
     }
     return s;
 }
+
+int isPossible(vector<int> &arr, int day, int k, int m)
+{
+    int n = arr.size();
+    int count = 0, noOfB = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] <= day)
+        {
+            count++;
+        }
+        else
+        {
+            noOfB += (count / k);
+            count = 0;
+        }
+    }
+    noOfB += (count / k);
+
+    return noOfB >= m;
+}
+int roseGarden(vector<int> &arr, int k, int m)
+{
+    int n = arr.size();
+    if (k * m > n)
+    {
+        return -1;
+    }
+    int maxi = INT_MIN, mini = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+        maxi = max(maxi, arr[i]);
+        mini = min(mini, arr[i]);
+    }
+
+    // Brute force - //O(n)+O(maxi-mini+1)*n
+
+    // for (int i = mini; i <= maxi; i++)
+    // {
+    //     if (isPossible(arr, i, k, m))
+    //     {
+    //         return i;
+    //     }
+    // }
+    // return -1;
+
+    // Binary Search appraoch - O(n)+Olog(maxi-mini+1)O(n)
+    int s = mini, e = maxi;
+    // int ans = -1;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+        if (isPossible(arr, mid, k, m))
+        {
+            // ans = mid;
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+    return s;
+}
 int main()
 {
-    vector<int> v = {7, 15, 6, 3};
-    int h = 8;
-    int ans = minimumRateToEatBananas(v, h);
-    cout << "Koko should eat atleast " << ans << " bananas/hr.\n";
+    // vector<int> v = {7, 15, 6, 3};
+    // int h = 8;
+    // int ans = minimumRateToEatBananas(v, h);
+    // cout << "Koko should eat atleast " << ans << " bananas/hr.\n";
+
+    vector<int> arr = {7, 7, 7, 7, 13, 11, 12, 7};
+    int k = 3;
+    int m = 2;
+    int ans = roseGarden(arr, k, m);
+    if (ans == -1)
+        cout << "We cannot make m bouquets.\n";
+    else
+        cout << "We can make bouquets on day " << ans << "\n";
     return 0;
 }
