0909b8cae0a3a45bc05ccc6e029215b7ef3e9fa5
Prathmesh <prathmeshparab12.o@gmail.com>
2024-05-30 23:17:27 +0530

    Done with the day 38

--- Binary_search_Revision/binary_search_on1D_array.cpp
+++ Binary_search_Revision/binary_search_on1D_array.cpp
@@ -859,6 +859,59 @@ int findPages(vector<int> &arr, int n, int m)
     }
     return low;
 }
+
+int countPartitions(vector<int> &a, int maxSum)
+{
+    int n = a.size(); // size of array.
+    int partitions = 1;
+    long long subarraySum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (subarraySum + a[i] <= maxSum)
+        {
+            // insert element to current subarray
+            subarraySum += a[i];
+        }
+        else
+        {
+            // insert element to next subarray
+            partitions++;
+            subarraySum = a[i];
+        }
+    }
+    return partitions;
+}
+
+int largestSubarraySumMinimized(vector<int> &a, int k)
+{
+    // int low = *max_element(a.begin(), a.end());
+    // int high = accumulate(a.begin(), a.end(), 0);
+
+    // for (int maxSum = low; maxSum <= high; maxSum++)
+    // {
+    //     if (countPartitions(a, maxSum) == k)
+    //         return maxSum;
+    // }
+    // return low;
+
+    int low = *max_element(a.begin(), a.end());
+    int high = accumulate(a.begin(), a.end(), 0);
+    // Apply binary search:
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int partitions = countPartitions(a, mid);
+        if (partitions > k)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
 int main()
 {
     // vector<int> arr = {2, 3, 5, 10, 13, 17};
@@ -953,10 +1006,15 @@ int main()
     // int ans = aggressiveCows(stalls, k);
     // cout << "The maximum possible minimum distance is: " << ans << "\n";
 
-    vector<int> arr = {25, 46, 28, 49, 24};
-    int n = 5;
-    int m = 4;
-    int ans = findPages(arr, n, m);
+    // vector<int> arr = {25, 46, 28, 49, 24};
+    // int n = 5;
+    // int m = 4;
+    // int ans = findPages(arr, n, m);
+    // cout << "The answer is: " << ans << "\n";
+
+    vector<int> a = {10, 20, 30, 40};
+    int k = 2;
+    int ans = largestSubarraySumMinimized(a, k);
     cout << "The answer is: " << ans << "\n";
     return 0;
 }
