d94c8c5f2e732171fdd7c2fa1fbfa8e69d38501a
Prathmesh <prathmeshparab12.o@gmail.com>
2024-06-20 22:51:28 +0530

    Done with day 59

--- Linked List/introDoublyLL.cpp
+++ Linked List/introDoublyLL.cpp
@@ -72,6 +72,46 @@ void insertAtTail(Node *&head, int val)
     temp->prev = tail;
     tail = temp;
 }
+
+void insertAtAnyPosition(Node *&head, int val, int pos)
+{
+    if (pos == 1)
+    {
+        insertAtHead(head, val);
+        return;
+    }
+
+    Node *temp = head;
+    int count = 1;
+
+    while (count < pos - 1)
+    {
+        temp = temp->next;
+        count++;
+    }
+
+    if (temp->next == NULL)
+    {
+        insertAtTail(head, val);
+        return;
+    }
+    Node *nodeToinsert = new Node(val);
+    nodeToinsert->next = temp->next;
+    temp->next->prev = nodeToinsert;
+    temp->next = nodeToinsert;
+    nodeToinsert->prev = temp;
+}
+void deleteNode(int position, Node *&head)
+{
+    if (position == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        temp->next->prev = nullptr;
+        temp->next = nullptr;
+        delete (temp);
+    }
+}
 int main()
 {
     vector<int> arr = {12, 45, 66, 80};
@@ -81,6 +121,10 @@ int main()
     insertAtHead(head, 40);
     traverseDLL(head);
     insertAtTail(head, 100);
+    traverseDLL(head);
+    insertAtAnyPosition(head, 3, 4);
+    traverseDLL(head);
+    deleteNode(1, head);
     traverseDLL(head);
     return 0;
 }
