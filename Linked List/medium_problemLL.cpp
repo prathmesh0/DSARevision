9c1f33dd775360a0fa324b21b67291419e02282d
Prathmesh <prathmeshparab12.o@gmail.com>
2024-06-21 23:40:10 +0530

    Done with day 60

--- Linked List/medium_problemLL.cpp
+++ Linked List/medium_problemLL.cpp
@@ -1,7 +1,124 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+Node *convertArraytoLL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *tail = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        tail->next = temp;
+        tail = temp;
+    }
+    return head;
+}
+void traverseLL(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+Node *findMiddle(Node *head)
+{
+    // Brute force approach
+    //  if (head == NULL || head->next == NULL)
+    //  {
+    //      return head;
+    //  }
+    //  Node *temp = head;
+    //  int count = 0;
+
+    // while (temp != NULL)
+    // {
+    //     count++;
+    //     temp = temp->next;
+    // }
+    // int mid = (count / 2) + 1;
+    // temp = head;
+    // while (temp != NULL)
+    // {
+    //     mid = mid - 1;
+    //     if (mid == 0)
+    //     {
+    //         break;
+    //     }
+    //     temp = temp->next;
+    // }
+    // return temp;
+
+    // optimal approach
+    Node *slow = head;
+    Node *fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+Node *reverseLinkedList(Node *&head)
+{
+    // Bruteforce
+    // Node *temp = head;
+    // stack<int> st;
+    // while (temp != NULL)
+    // {
+    //     st.push(temp->data);
+    //     temp = temp->next;
+    // }
+
+    // temp = head;
+    // while (!st.empty())
+    // {
+    //     temp->data = st.top();
+    //     st.pop();
+    //     temp = temp->next;
+    // }
+    // return temp;
+
+    // optimal approch
+    Node *prev = NULL;
+    while (head != NULL)
+    {
+        Node *next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
+
+int main()
+{
+    vector<int> num = {1, 2, 3, 4, 5, 6};
+    Node *head = convertArraytoLL(num);
+
+    // Node *middle = findMiddle(head);
+    // cout << "The middle value " << middle->data << endl;
+
+    Node *temp = reverseLinkedList(head);
+    traverseLL(temp);
+
     return 0;
 }
