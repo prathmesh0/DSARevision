368d277d146ad0661bbdf7f6c4f676cbefedbbc1
Prathmesh <prathmeshparab12.o@gmail.com>
2024-06-24 23:16:25 +0530

    Done with day 63

--- Linked List/medium_problemLL.cpp
+++ Linked List/medium_problemLL.cpp
@@ -149,38 +149,127 @@ bool detectLoop(Node *head)
 
     return false;
 }
-bool isPalindrome(Node *head)
+
+Node *detectCycle(Node *head)
 {
+    // if (head == NULL || head->next == NULL)
+    //     return NULL;
+    // Node *temp = head;
+    // unordered_map<Node *, int> mpp;
+    // while (temp != NULL)
+    // {
+    //     if (mpp.find(temp) != mpp.end())
+    //     {
+    //         return temp;
+    //     }
+    //     mpp[temp] = 1;
+    //     temp = temp->next;
+    // }
+    // return NULL;
 
-    // stack<int> st;
+    // Optimal
+    if (head == NULL || head->next == NULL)
+        return NULL;
+    Node *slow = head;
+    Node *fast = head;
+    Node *temp = head;
 
+    while (fast->next != NULL && fast->next->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            while (temp != slow)
+            {
+                temp = temp->next;
+                slow = slow->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
+
+int lengthOfLoop(Node *slow, Node *fast)
+{
+    int count = 1;
+    fast = fast->next;
+    while (slow != fast)
+    {
+        count++;
+        fast = fast->next;
+    }
+    return count;
+}
+int countNodesinLoop(struct Node *head)
+{
+    // if (head == NULL || head->next == NULL)
+    //     return 0;
+    // unordered_map<Node *, int> mpp;
+    // int length = 1;
     // Node *temp = head;
 
     // while (temp != NULL)
     // {
-
-    //     st.push(temp->data);
-
-    //     temp = temp->next;
-    // }
-
-    // temp = head;
-
-    // while (temp != NULL)
-    // {
-    //     if (temp->data != st.top())
+    //     if (mpp.find(temp) != mpp.end())
     //     {
-
-    //         // it's not a palindrome
-    //         return false;
+    //         return (length - mpp[temp]);
     //     }
-
-    //     st.pop();
-
+    //     mpp[temp] = length;
+    //     length += 1;
     //     temp = temp->next;
     // }
 
-    // return true;
+    if (head == NULL || head->next == NULL)
+        return 0;
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast->next != NULL && fast->next->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            return lengthOfLoop(slow, fast);
+        }
+    }
+
+    return 0;
+}
+bool isPalindrome(Node *head)
+{
+
+    stack<int> st;
+
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+
+        st.push(temp->data);
+
+        temp = temp->next;
+    }
+
+    temp = head;
+
+    while (temp != NULL)
+    {
+        if (temp->data != st.top())
+        {
+
+            // it's not a palindrome
+            return false;
+        }
+
+        st.pop();
+
+        temp = temp->next;
+    }
+
+    return true;
 
     if (head == NULL || head->next == NULL)
     {
@@ -198,8 +287,8 @@ bool isPalindrome(Node *head)
 }
 int main()
 {
-    vector<int> num = {1, 2, 3, 4, 5, 6};
-    Node *head = convertArraytoLL(num);
+    // vector<int> num = {1, 2, 3, 4, 5, 6};
+    // Node *head = convertArraytoLL(num);
 
     // Node *middle = findMiddle(head);
     // cout << "The middle value " << middle->data << endl;
@@ -216,6 +305,36 @@ int main()
     //     cout << "No loop detected in the linked list." << endl;
     // }
 
-    cout << isPalindrome(head);
+    // cout << isPalindrome(head);
+
+    Node *node1 = new Node(1);
+    Node *node2 = new Node(2);
+    node1->next = node2;
+    Node *node3 = new Node(3);
+    node2->next = node3;
+    Node *node4 = new Node(4);
+    node3->next = node4;
+    Node *node5 = new Node(5);
+    node4->next = node5;
+
+    // Make a loop from node5 to node2
+    node5->next = node2;
+
+    // Set the head of the linked list
+    Node *head = node1;
+
+    // Detect the loop in the linked list
+    // Node *loopStartNode = detectCycle(head);
+
+    // if (loopStartNode)
+    // {
+    //     cout << "Loop detected. Starting node of the loop is: " << loopStartNode->data << endl;
+    // }
+    // else
+    // {
+    //     cout << "No loop detected in the linked list." << endl;
+    // }
+
+    cout << countNodesinLoop(head) << endl;
     return 0;
 }
