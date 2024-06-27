8de0484939579efeb03af228b0426797e4d32c85
Prathmesh <prathmeshparab12.o@gmail.com>
2024-06-27 23:29:39 +0530

    Done with day 66

--- Linked List/medium_problemLL.cpp
+++ Linked List/medium_problemLL.cpp
@@ -521,10 +521,167 @@ Node *sortList(Node *head)
     Node *r = sortList(slow);
     return mergeList(l, r);
 }
+
+Node *segregate(Node *head)
+{
+
+    // Node *temp = head;
+    // int count0 = 0, count1 = 0, count2 = 0;
+    // while (temp != NULL)
+    // {
+    //     if (temp->data == 0)
+    //     {
+    //         count0++;
+    //     }
+    //     else if (temp->data == 1)
+    //     {
+    //         count1++;
+    //     }
+    //     else
+    //     {
+    //         count2++;
+    //     }
+    //     temp = temp->next;
+    // }
+
+    // temp = head;
+    // while (temp != NULL)
+    // {
+    //     if (count0)
+    //     {
+    //         temp->data = 0;
+    //         count0--;
+    //     }
+    //     else if (count1)
+    //     {
+    //         temp->data = 1;
+    //         count1--;
+    //     }
+    //     else
+    //     {
+    //         temp->data = 2;
+    //         count2--;
+    //     }
+    //     temp = temp->next;
+    // }
+    // return head;
+
+    Node *zeroHead = new Node(-1);
+    Node *oneHead = new Node(-1);
+    Node *twoHead = new Node(-1);
+    Node *zero = zeroHead;
+    Node *one = oneHead;
+    Node *two = twoHead;
+
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == 0)
+        {
+            zero->next = temp;
+            zero = temp;
+        }
+        else if (temp->data == 1)
+        {
+            one->next = temp;
+            one = temp;
+        }
+        else
+        {
+            two->next = temp;
+            two = temp;
+        }
+        temp = temp->next;
+    }
+
+    zero->next = oneHead->next ? oneHead->next : twoHead->next;
+    one->next = twoHead->next;
+    two->next = NULL;
+
+    Node *newHead = zeroHead->next;
+    delete (zeroHead);
+    delete (oneHead);
+    delete (twoHead);
+    return newHead;
+}
+
+Node *reverse(Node *head)
+{
+    Node *prev = NULL;
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        Node *nex = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nex;
+    }
+    return prev;
+}
+int helper(Node *temp)
+{
+    if (temp == NULL)
+    {
+        return 1;
+    }
+
+    int carry = helper(temp->next);
+    temp->data += carry;
+    if (temp->data < 10)
+    {
+        return 0;
+    }
+    temp->data = 0;
+    return 1;
+}
+Node *addOne(Node *head)
+{
+    // head = reverse(head);
+    // Node *temp = head;
+    // int carry = 1;
+
+    // while (temp != NULL)
+    // {
+    //     temp->data += carry;
+
+    //     if (temp->data < 10)
+    //     {
+    //         carry = 0;
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         carry = 1;
+    //         temp->data = 0;
+    //     }
+    //     temp = temp->next;
+    // }
+
+    // if (carry == 1)
+    // {
+    //     Node *newNode = new Node(1);
+    //     head = reverse(head);
+    //     newNode->next = head;
+    //     return newNode;
+    // }
+
+    // head = reverse(head);
+    // return head;
+
+    int carry = helper(head);
+
+    if (carry == 1)
+    {
+        Node *newNode = new Node(1);
+        newNode->next = head;
+        return newNode;
+    }
+    return head;
+}
 int main()
 {
-    vector<int> num = {1, 2, 3, 4, 5, 6};
-    Node *head = convertArraytoLL(num);
+    // vector<int> num = {0, 2, 2, 2, 1, 2, 1, 0};
+    // Node *head = convertArraytoLL(num);
 
     // Node *middle = findMiddle(head);
     // cout << "The middle dataue " << middle->data << endl;
@@ -582,7 +739,15 @@ int main()
     // Node *ans = deleteMiddle(head);
     // traverseLL(ans);
 
-    Node *ans = sortList(head);
+    // Node *ans = sortList(head);
+    // traverseLL(ans);
+
+    // Node *count = segregate(head);
+    // traverseLL(count);
+
+    vector<int> num1 = {1, 5, 9};
+    Node *head = convertArraytoLL(num1);
+    Node *ans = addOne(head);
     traverseLL(ans);
 
     return 0;
