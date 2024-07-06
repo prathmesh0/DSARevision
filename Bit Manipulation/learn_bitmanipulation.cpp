ec295d46a04af9aa6b4d506acec679a7d1dfe110
Prathmesh <prathmeshparab12.o@gmail.com>
2024-07-06 22:52:26 +0530

    Done with day 75

--- Bit Manipulation/learn_bitmanipulation.cpp
+++ Bit Manipulation/learn_bitmanipulation.cpp
@@ -183,6 +183,73 @@ int divide(int dividend, int divisor)
     }
     return sign ? quotient : -quotient;
 }
+
+int minBitFlips(int start, int goal)
+{
+    int ans = start ^ goal;
+    int cnt = 0;
+
+    while (ans != 0)
+    {
+        ans = ans & (ans - 1);
+        cnt++;
+    }
+    return cnt;
+}
+
+vector<vector<int>> subsets(vector<int> &nums)
+{
+    vector<vector<int>> ans;
+    int n = nums.size();
+    for (int num = 0; num <= (1 << n) - 1; num++)
+    {
+        vector<int> ds;
+        for (int i = 0; i <= (n - 1); i++)
+        {
+            // check the bit is set or not
+            if (num & (1 << i))
+            {
+                ds.push_back(nums[i]);
+            }
+        }
+        ans.push_back(ds);
+    }
+    return ans;
+}
+int xorfrom1Ton(int n)
+{
+    // Brute force approach
+    //  int ans = 0;
+    //  for (int i = 1; i <= n; i++)
+    //  {
+    //      ans = ans ^ i;
+    //  }
+    //  return ans;
+
+    // Optimal approach
+    if (n % 4 == 1)
+        return 1;
+    else if (n % 4 == 2)
+        return n + 1;
+    else if (n % 4 == 3)
+        return 0;
+    else
+        return n;
+}
+int findXOR(int l, int r)
+{
+    return xorfrom1Ton(l - 1) ^ xorfrom1Ton(r);
+}
+
+int singleNumber(vector<int> &nums)
+{
+    int ans = 0;
+    for (auto it : nums)
+    {
+        ans = ans ^ it;
+    }
+    return ans;
+}
 int main()
 {
 
@@ -202,7 +269,22 @@ int main()
     // cout << isPowerOfTwo(16) << endl;
     // cout << oddEven(8) << endl;
     // cout << countSetBits(13) << endl;
-    cout << divide(22, 3) << endl;
+    // cout << divide(22, 3) << endl;
+    // cout << minBitFlips(10, 7) << endl;
+    vector<int> arr = {1, 2, 3, 2, 3};
+    // vector<vector<int>> ans = subsets(arr);
+    // for (auto it : ans)
+    // {
+    //     for (auto i : it)
+    //     {
+    //         cout << i << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // cout << xorfrom1Ton(7) << endl;
+    // cout << findXOR(4, 8) << endl;
+    cout << singleNumber(arr) << endl;
 
     return 0;
 }
