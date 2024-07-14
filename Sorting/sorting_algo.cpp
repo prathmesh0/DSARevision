e6e36634468abea5d9982541b0841810607b0582
Prathmesh <prathmeshparab12.o@gmail.com>
2024-07-14 23:38:50 +0530

    Done with day 83

--- Sorting/sorting_algo.cpp
+++ Sorting/sorting_algo.cpp
@@ -36,11 +36,83 @@ void bubble_sort(vector<int> &arr)
     }
 }
 
+void insertion_sort(vector<int> &arr)
+{
+    int n = arr.size();
+    for (int i = 0; i <= n - 1; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+}
+
+void merge(vector<int> &arr, int low, int mid, int high)
+{
+    vector<int> temp;    // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1; // starting index of right half of arr
+
+    // storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+        return;
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);      // left half
+    mergeSort(arr, mid + 1, high); // right half
+    merge(arr, low, mid, high);    // merging sorted halves
+}
 int main()
 {
     vector<int> v = {13, 44, 22, 1, 33, 55};
+    int n = v.size();
     // selectionSort(v);
-    bubble_sort(v);
+    // bubble_sort(v);
+    // insertion_sort(v);
+    mergeSort(v, 0, n - 1);
     cout << "Element after sorting" << endl;
     for (auto it : v)
     {
