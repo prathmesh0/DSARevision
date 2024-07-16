ff35b6b6613a114fa7fe28dc1fec887abdc089d6
Prathmesh <prathmeshparab12.o@gmail.com>
2024-07-16 22:59:01 +0530

    done with day 85

--- Sorting/sorting_algo.cpp
+++ Sorting/sorting_algo.cpp
@@ -143,6 +143,45 @@ void insertion_sort(int arr[], int i, int n)
 
     insertion_sort(arr, i + 1, n);
 }
+int partition(vector<int> &arr, int low, int high)
+{
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j)
+    {
+        while (arr[i] <= pivot && i <= high - 1)
+        {
+            i++;
+        }
+
+        while (arr[j] > pivot && j >= low + 1)
+        {
+            j--;
+        }
+        if (i < j)
+            swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void qs(vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
+}
+
+vector<int> quickSort(vector<int> &arr)
+{
+    qs(arr, 0, arr.size() - 1);
+    return arr;
+}
 
 int main()
 {
@@ -151,7 +190,8 @@ int main()
     // selectionSort(v);
     // bubble_sort(v);
     // insertion_sort(v);
-    mergeSort(v, 0, n - 1);
+    // mergeSort(v, 0, n - 1);
+    quickSort(v);
     cout << "Element after sorting" << endl;
     for (auto it : v)
     {
