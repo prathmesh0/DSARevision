fcc376005860983261c90c495aa2fde00cac5b48
Prathmesh <prathmeshparab12.o@gmail.com>
2024-07-21 23:13:28 +0530

    Done with day 90

--- Stack and Queues/learning.cpp
+++ Stack and Queues/learning.cpp
@@ -248,7 +248,7 @@ public:
 bool isValid(string s)
 {
     stack<char> st;
-    for (auto it : s)  //traverse string
+    for (auto it : s) // traverse string
     {
         if (it == '(' || it == '{' || it == '[')
             st.push(it);
@@ -267,6 +267,71 @@ bool isValid(string s)
     return st.empty();
 }
 
+struct stackNode
+{
+    int data;
+    stackNode *next;
+    int size;
+    stackNode(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+struct stack
+{
+    stackNode *top;
+    int size;
+    stack()
+    {
+        top = NULL;
+        size = 0;
+    }
+    void stackPush(int x)
+    {
+        stackNode *element = new stackNode(x);
+        element->next = top;
+        top = element;
+        cout << "Element pushed" << "\n";
+        size++;
+    }
+    int stackPop()
+    {
+        if (top == NULL)
+        {
+            return -1;
+        }
+        int topData = top->data;
+        stackNode *temp = top;
+        top = top->next;
+        delete temp;
+        size--;
+        return topData;
+    }
+    int stackSize()
+    {
+        return size;
+    }
+    bool stackIsEmpty()
+    {
+        return top == NULL;
+    }
+    int stackPeek()
+    {
+        if (top == NULL)
+            return -1;
+        return top->data;
+    }
+    void printStack()
+    {
+        stackNode *current = top;
+        while (current != NULL)
+        {
+            cout << current->data << " ";
+            current = current->next;
+        }
+    }
+};
 int main()
 {
     // StackImplementation s;
