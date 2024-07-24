2324d08429ebceb9a59153c5163ad376bf1a904b
Prathmesh <prathmeshparab12.o@gmail.com>
2024-07-24 23:08:23 +0530

    Done with day 93

--- Stack and Queues/usingLL.cpp
+++ Stack and Queues/usingLL.cpp
@@ -265,7 +265,24 @@ public:
         return mini;
     }
 };
+queue<int> rev(queue<int> q)
+{
+    stack<int> st;
+    while (!q.empty())
+    {
+        int ele = q.front();
+        q.pop();
+        st.push(ele);
+    }
 
+    while (!st.empty())
+    {
+        int ele = st.top();
+        st.pop();
+        q.push(ele);
+    }
+    return q;
+}
 int main()
 {
     // Stack s;
